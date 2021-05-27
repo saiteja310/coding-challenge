1fa756866ef8ae4b0865c443d43eac9adad20bc1
saiteja310 <saiteja310@gmail.com>
2021-05-27 23:43:17 +0530

    DiscountCouponCode Generator

---
+++ CourierService.Tests/CouponCodeGenerators/CouponCodeGeneratorTests.cs
@@ -0,0 +1,42 @@
+﻿using CourierService.CouponCodeGenerator;
+using CourierService.Interfaces;
+using CourierService.Models.Enum;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourierService.Tests.CouponCodeGenerators
+{
+    [TestFixture]
+    class CouponCodeGeneratorTests
+    {
+        [TestCase("OFR001", true)]
+        [TestCase("OFR002", true)]
+        [TestCase("OFR003", true)]
+        [TestCase("OFR004", false)]
+        [TestCase(null, false)]
+        public void TestStaticDiscountCouponCodeGenerator(string key, bool expected)
+        {
+            IDiscountCouponCodeGenerator generator = new StaticDiscountCouponCodeGenerator();
+            var result = generator.GenerateDiscountCouponCodes();
+            Assert.AreEqual(3, result.Count);
+            Assert.AreEqual(expected, result.ContainsKey(key));
+        }
+
+        [TestCase("OFR001", 10, DiscountCouponType.PERCENT)]
+        [TestCase("OFR002", 7, DiscountCouponType.PERCENT)]
+        [TestCase("OFR003", 5, DiscountCouponType.PERCENT)]
+        public void TestStaticDiscountCouponCodeGeneratorValue(string key, decimal expectedValue, DiscountCouponType type)
+        {
+            IDiscountCouponCodeGenerator generator = new StaticDiscountCouponCodeGenerator();
+            var result = generator.GenerateDiscountCouponCodes();
+            Assert.IsTrue(result.ContainsKey(key));
+            Assert.AreEqual(key, result[key].Code);
+            Assert.AreEqual(expectedValue, result[key].Value);
+            Assert.AreEqual(type, result[key].DiscountCouponType);
+        }
+    }
+}
---
+++ CourierService/CouponCodeGenerator/StaticDiscountCouponCodeGenerator.cs
@@ -0,0 +1,37 @@
+﻿using CourierService.Implementation.DiscountValidationRules;
+using CourierService.Interfaces;
+using CourierService.Models;
+using CourierService.Models.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourierService.CouponCodeGenerator
+{
+    public class StaticDiscountCouponCodeGenerator : IDiscountCouponCodeGenerator
+    {
+        public IDictionary<string, DiscountCouponCode> GenerateDiscountCouponCodes()
+        {
+            var couponCodes = new List<DiscountCouponCode>();
+            couponCodes.Add(new DiscountCouponCode("OFR001", 10, DiscountCouponType.PERCENT, new List<IDiscountValidationRule>()
+            {
+                DistanceValidationRulesFactory.CreateLessThanDistanceRule(200),
+                WeightValidationRulesFactory.CreateInBetweenWeightRule(70, 200)
+            }));
+            couponCodes.Add(new DiscountCouponCode("OFR002", 7, DiscountCouponType.PERCENT, new List<IDiscountValidationRule>()
+            {
+                DistanceValidationRulesFactory.CreateInBetweenDistanceRule(50, 150),
+                WeightValidationRulesFactory.CreateInBetweenWeightRule(100, 250)
+            }));
+            couponCodes.Add(new DiscountCouponCode("OFR003", 5, DiscountCouponType.PERCENT, new List<IDiscountValidationRule>()
+            {
+                DistanceValidationRulesFactory.CreateInBetweenDistanceRule(50, 250),
+                WeightValidationRulesFactory.CreateInBetweenWeightRule(10, 150)
+            }));
+
+            return couponCodes.ToDictionary(e => e.Code, e => e);
+        }
+    }
+}
